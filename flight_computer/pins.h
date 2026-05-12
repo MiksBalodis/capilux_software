2e8c2bda556fe994da2402e3296cdf1397f77873
MiksBalodis <Miksbalodis2008@gmail.com>
2026-05-13 00:08:32 +0300

    added initial mission state logic

---
+++ flight_computer/pins.h
@@ -0,0 +1,37 @@
+
+#pragma once
+
+#define PIN_UART_TX      0
+#define PIN_UART_RX      1
+
+#define PIN_I2C2_SDA     2
+#define PIN_I2C2_SCL     3
+#define PIN_I2C1_SDA     4
+#define PIN_I2C1_SCL     5
+
+#define PIN_VIDEO_A0     6
+#define PIN_LED_EN1      7
+
+#define PIN_CAM_TX       8
+#define PIN_CAM_RX       9
+#define PIN_CAM_EN       10
+
+#define PIN_LED_EN2      11
+#define PIN_LED_EN3      12
+#define PIN_LED_EN4      13
+
+#define PIN_SOE          14
+#define PIN_LO           15
+
+#define PIN_SD_MISO      16
+#define PIN_SD_CS        17
+#define PIN_SD_CLK       18
+#define PIN_SD_MOSI      19
+
+#define PIN_SRCLK        20
+#define PIN_RCLK         21
+#define PIN_OE           22
+#define PIN_SER          26
+
+#define PIN_VIDEO_A1     27
+#define PIN_VIDEO_A2     28
